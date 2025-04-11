45d93ee516b1e12f9a60f15cf7cda610b0528449
ratzz64 <ratbinrhn@gmail.com>
2025-04-11 12:26:38 +0600

    github workflow

--- abc.py
+++ abc.py
@@ -1,2 +1,11 @@
 print("THIS IS ABC.py")
 print("added this new line")
+
+Github Workflow:
+git add . (stagging)
+git commit -m "your msg"
+git push -u origin main
+
+always remember to save the file
+
+Bye
